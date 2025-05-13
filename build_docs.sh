b0e284c0928df3f488c214252256b11f4ddee958
billyfish <tyrrell.simon@gmail.com>
2025-05-13 07:53:51 +0100

    now does dsw folder too

--- build_docs.sh
+++ build_docs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# run as ./build_docs.sh <OUTPUT DIR>
+
 for filename in pages/*.html
 do
   echo "file $filename"
@@ -10,3 +12,20 @@ do
 	cat partials/header.html ${filename} partials/footer.html > ${base_name}
 done
 
+
+for filename in pages/dsw/*.html
+do
+  echo "file $filename"
+
+	base_name=$(basename ${filename})
+	echo "base ${base_name}"
+
+	cat partials/header.html ${filename} partials/footer.html > dsw/${base_name}
+done
+
+echo "Copying files to $1"
+cp *.html $1
+
+
+mkdir -p $1/dsw
+cp dsw/*.html $1/dsw/
